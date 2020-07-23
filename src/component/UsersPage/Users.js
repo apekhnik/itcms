390dcaf0032e0f39bdcbade32e476d7ca31158b3
apekhnik <iam3ji0@gmail.com>
2020-07-23 12:44:23 +0400

    move 2 container into 1 file

--- src/component/UsersPage/Users.js
+++ src/component/UsersPage/Users.js
@@ -1,39 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
 import UserItem from "./UserItem";
-class Users extends Component {
+const Users =(props)=> {
  
-  render() {
+  
     const pagesCount = Math.ceil(
-      this.props.totalUsersCount / this.props.pageSize
+      props.totalUsersCount / props.pageSize
     );
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) {
       pages.push(i);
     }
-console.log(this.props)
     return (
       <div>
         {pages.map((i) => (
           <span
-            style={this.props.currentPage === i ? { color: "red" } : {}}
-            onClick={() => this.props.onCurrentChange(i)}
+            style={props.currentPage === i ? { color: "red" } : {}}
+            onClick={() => props.onCurrentChange(i)}
           >
             {i}
           </span>
         ))}
-        {this.props.users.map((us, index) => {
+        {props.users.map((us, index) => {
           return (
             <UserItem
               user={us}
-              follow={this.props.follow}
-              unfollow={this.props.unfollow}
-              toggle={this.props.toggle}
+              follow={props.follow}
+              unfollow={props.unfollow}
+              toggle={props.toggle}
               key={us.id}
             />
           );
         })}
       </div>
     );
-  }
+  
 }
 export default Users;
