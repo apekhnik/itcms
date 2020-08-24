8891388d3646998a036317ef16f8d2d99c425e81
apekhnik <iam3ji@gmail.com>
2020-08-24 10:39:46 +0300

    add awesome icon to validate form

--- src/forms/form-controls/Textarea.jsx
+++ src/forms/form-controls/Textarea.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import classes from "classnames";
 import style from "./Textarea.module.css";
+import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+const errorDiv = (
+  <div className={style.errorDiv}>
+    <FontAwesomeIcon icon={faExclamationCircle} />
+  </div>
+);
 export const Textarea = ({
   input,
   placeholder,
   type,
   meta: { touched, error, warning },
 }) => {
-  const hasError = touched && error ? <span>{error}</span> : null;
+  const hasError = touched && error ? errorDiv : null;
   const cl = classes(style.formControl, hasError ? style.error : "");
 
   return (
@@ -23,7 +30,7 @@ export const FormInput = ({
   type,
   meta: { touched, error, warning },
 }) => {
-  const hasError = touched && error ? <span>{error}</span> : null;
+  const hasError = touched && error ? errorDiv : null;
   const cl = classes(style.formControl, hasError ? style.error : "");
 
   return (
@@ -31,5 +38,19 @@ export const FormInput = ({
       <input {...input} placeholder={placeholder} />
       <div>{hasError}</div>
     </div>
+    // <FormControls
+    //   className={cl}
+    //   hasError={hasError}
+    //   {...input}
+    //   placeholder={placeholder}
+    // />
   );
 };
+// const FormControls = (props) => {
+//   return (
+//     <div className={props.className}>
+//       <input {...props.input} placeholder={props.placeholder} />
+//       <div>{props.hasError}</div>
+//     </div>
+//   );
+// };
