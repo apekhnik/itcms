a2db83b8a6e56976b63d968466b12e807d22bbb8
apekhnik <iam3ji@gmail.com>
2020-08-26 17:52:04 +0300

    whant to laptop

--- src/component/Header/LoginIndicator.js
+++ src/component/Header/LoginIndicator.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import style from "./Header.module.css";
-const LoginIndicator = ({ login, isAuth, id }) => {
+const LoginIndicator = ({ login, isAuth, id, userLogout }) => {
   const linkToLogin = (
     <NavLink to={"/login"} className={style.auth_block}>
       Login
     </NavLink>
   );
-  const linkToProfile = <NavLink to={`/profile/${id}`}>{login}</NavLink>;
+  const logoutBtn = <button onClick={() => userLogout()}>logout</button>;
+  const linkToProfile = (
+    <NavLink to={`/profile/${id}`}>
+      {login},{logoutBtn}
+    </NavLink>
+  );
 
   if (!isAuth) return linkToLogin;
   return <div className={style.auth_block}>Hello, {linkToProfile}</div>;
