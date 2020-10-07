1ec90d426546226ed60808880db2628b6851218d
apekhnik <iam3ji@gmail.com>
2020-10-07 17:22:32 +0300

    Updates

---
+++ precache-manifest.8eb803e5eb46b966aafd8a1560fb4664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5de8afb916f63347835b7377168382c",
+    "url": "/itcms/index.html"
+  },
+  {
+    "revision": "01d4aa69510c0a85b7bf",
+    "url": "/itcms/static/css/main.33a2aa45.chunk.css"
+  },
+  {
+    "revision": "5e3fa93be6a853ab51ce",
+    "url": "/itcms/static/js/2.62d9d1ee.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/itcms/static/js/2.62d9d1ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01d4aa69510c0a85b7bf",
+    "url": "/itcms/static/js/main.47d636f9.chunk.js"
+  },
+  {
+    "revision": "d8639faad16c88fb96c6",
+    "url": "/itcms/static/js/runtime-main.6a6b0314.js"
+  }
+]);
