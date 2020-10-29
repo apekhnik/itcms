39187f363c4f7c83244eda3856ae22db4ba72ad5
apekhnik <iam3ji0@gmail.com>
2020-10-29 10:21:25 +0300

    some body

--- src/component/Account/Account.js
+++ src/component/Account/Account.js
@@ -1,12 +1,10 @@
-import React, {useState} from "react";
+import React from "react";
 import Avatar from "../Avatar/Avatar";
-import UserInfo from "../UserInfo/UserInfo";
 import style from "./Account.module.css";
 import Loader from "../Loader/Lodaer";
-import ProfileStatus from '../ProfileStatus/ProfileStatus'
-import ProfileDataChangePage from '../ProfilePage/ProfileDataForm'
+import ProfileDescription from "../ProfilePage/ProfileDescription";
+
 const Account = (props) => {
-  const [editMode, setEditMode] = useState(false)
   const loadPhoto = (e) => {
     if (e.target.files.length) {
       props.savePhoto(e.target.files[0]);
@@ -16,33 +14,18 @@ const Account = (props) => {
   const fakeAva =
     "https://cs10.pikabu.ru/post_img/big/2018/03/18/1/1521324516155263949.png";
   if (!props.profile) return <Loader />;
-  
+
   const ava = props.profile.photos.large || fakeAva;
 
   return (
     <div className={style.profile}>
-      <Avatar src={ava} isOwner={props.isOwner} loadPhoto={loadPhoto} savePhoto={props.savePhoto}/>
-      <div className={style.profileDescription}>
-      <h1>{props.profile.fullName}</h1>
-      <ProfileStatus
-        status={props.status}
-        updateStatus={props.updateStatus}
-        inputUserStatusChange={props.inputUserStatusChange}
+      <Avatar
+        src={ava}
+        isOwner={props.isOwner}
+        loadPhoto={loadPhoto}
+        savePhoto={props.savePhoto}
       />
-      {editMode ? <ProfileDataChangePage/> :       <UserInfo
-        name={props.profile.fullName}
-        lookingForAJob={props.profile.lookingForAJob}
-        lookingForAJobDescription={props.profile.lookingForAJobDescription}
-        aboutMe={props.profile.aboutMe}
-        contacts={props.profile.contacts}
-      />}
-      </div>
-
-      
-
-      <button onClick={()=>setEditMode(!editMode)}>{editMode ? 'save': 'edit'}</button>
-      
-
+      <ProfileDescription {...props} />
     </div>
   );
 };
---
+++ src/component/ProfilePage/ProfileDescription.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import ProfileStatus from "../ProfileStatus/ProfileStatus";
+import ProfileDataChangePage from "./ProfileDataForm";
+import UserInfo from "../UserInfo/UserInfo";
+import style from './Profile.module.css'
+const ProfileDescription = (props) => {
+  const [isEdit, setisEdit] = useState(false);
+
+  return (
+    <div className={style.profileDescription}>
+    <h1>{props.profile.fullName}</h1>
+      <ProfileStatus
+        status={props.status}
+        updateStatus={props.updateStatus}
+        inputUserStatusChange={props.inputUserStatusChange}
+      />
+      <hr/>
+      {isEdit ? (
+        <ProfileDataChangePage />
+      ) : (
+        <UserInfo
+          name={props.profile.fullName}
+          lookingForAJob={props.profile.lookingForAJob}
+          lookingForAJobDescription={props.profile.lookingForAJobDescription}
+          aboutMe={props.profile.aboutMe}
+          contacts={props.profile.contacts}
+        />
+      )}
+      <button onClick={() => setisEdit(!isEdit)}>
+        {isEdit ? "save" : "edit"}
+      </button>
+    </div>
+  );
+};
+export default ProfileDescription;
