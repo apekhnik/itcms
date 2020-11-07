56f664ab07f36655380e149159f90fe6df096615
apekhnik <iam3ji0@gmail.com>
2020-11-07 11:59:55 +0300

    captcha

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { userLogin } from "../../redux/reducers/authReducer";
 import { Field, reduxForm } from "redux-form";
-import { FormInput } from "../../forms/form-controls/Textarea";
+import { FormInput, createField } from "../../forms/form-controls/Textarea";
 import style from "./Login.module.css";
 import { connect } from "react-redux";
 import { required } from "../../forms/form-validator/validator";
@@ -10,45 +10,52 @@ const Login = ({ userLogin, isAuth, captcha }) => {
   const onSubmit = (data) => {
     userLogin(data);
   };
-  
+
   if (isAuth) return <Redirect to="/profile" />;
   return (
     <div style={{ textAlign: "center" }}>
       <h1>LOGIN</h1>
-      <LoginReduxForm onSubmit={onSubmit} captcha={captcha}/>
+      <LoginReduxForm onSubmit={onSubmit} captcha={captcha} />
     </div>
   );
 };
 
 const LoginForm = (props) => {
-  console.log(props.captcha)
+  const antiBot = createField(
+    FormInput,
+    "text",
+    "Anti-bot",
+    "captcha",
+    [required],
+    "",
+    ""
+  );
+  const login = createField(
+    FormInput,
+    "text",
+    "Login",
+    "email",
+    [required],
+    "",
+    "Login"
+  );
+  const password = createField(
+    FormInput,
+    "text",
+    "Password",
+    "password",
+    [required],
+    "",
+    "Password"
+  );
   return (
     <form className={style.loginForm} onSubmit={props.handleSubmit}>
-      <Field
-        placeholder={"login"}
-        component={FormInput}
-        name={"email"}
-        validate={[required]}
-        className={style.loginForm_input}
-      />
+      {login}
+      {password}
 
-      <Field
-        placeholder={"password"}
-        component={FormInput}
-        name={"password"}
-        type={"password"}
-        validate={[required]}
-        className={style.loginForm_input}
-      />
-      {props.captcha && <img src={props.captcha}/>}
-      {props.captcha && <Field
-        placeholder={"anti-bot"}
-        component={FormInput}
-        name={"captcha"}
-        type={"text"}
-        validate={[required]}
-        className={style.loginForm_input}
-      />}
+      {props.captcha && <img src={props.captcha} />}
+      {props.captcha && antiBot}
+
       <div>
         <Field component={"input"} type={"checkbox"} name={"rememberMe"} />{" "}
         remember me
@@ -66,7 +73,7 @@ const LoginForm = (props) => {
 };
 const mapStateToProps = (state) => ({
   isAuth: state.auth.isAuth,
-  captcha: state.auth.captcha
+  captcha: state.auth.captcha,
 });
 const LoginReduxForm = reduxForm({ form: "login" })(LoginForm);
 export default connect(mapStateToProps, { userLogin })(Login);
