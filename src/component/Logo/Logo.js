0c0ed38abb6d6fb13ab664c911e5746947da0c0f
apekhnik <iam3ji0@gmail.com>
2020-11-14 11:01:05 +0300

    some pretty

--- src/component/Logo/Logo.js
+++ src/component/Logo/Logo.js
@@ -2,7 +2,7 @@ import React from "react";
 import style from './Logo.module.css'
 const Logo = () => {
   return <div className={style.logo}>
-    
+    <img src='https://c7.hotpng.com/preview/867/960/5/samurai-logo-art-samurai.jpg' alt=''/>
   </div>;
 };
 export default Logo;
